b879dee4ea25e9f1cbff9a904cdd557675fcaa71
Thiago Ádril <thiagoadril@gmail.com>
2020-05-10 15:00:55 -0300

    up

--- docker/scripts/configure-network.sh
+++ docker/scripts/configure-network.sh
@@ -3,5 +3,5 @@
 # SET network -> NETWORK_NAME=my_local_network
 
 if [ -z $(docker network ls --filter name=^$NETWORK_NAME --format="{{ .Name }}") ] ; then 
-     docker network create $NETWORK_NAME ; 
+     docker network create --driver bridge $NETWORK_NAME
 fi
