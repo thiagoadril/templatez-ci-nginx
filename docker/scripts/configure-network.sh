83dd7c820ff71ec7d1ad770ed068587ad8eff0f7
Thiago Ádril <thiagoadril@gmail.com>
2020-05-10 14:56:22 -0300

    updated

--- docker/scripts/configure-network.sh
+++ docker/scripts/configure-network.sh
@@ -2,6 +2,6 @@
 
 # SET network -> NETWORK_NAME=my_local_network
 
-if [ -z $(docker network ls --filter name=^${NETWORK_NAME}$ --format="{{ .Name }}") ] ; then 
-     docker network create ${NETWORK_NAME} ; 
+if [ -z $(docker network ls --filter name=^$NETWORK_NAME --format="{{ .Name }}") ] ; then 
+     docker network create $NETWORK_NAME ; 
 fi
